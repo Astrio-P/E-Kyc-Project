beecd18c65efe7133c525ded478b5aff275a780a
roy prio <priosalley@gmail.com>
2021-12-26 17:15:08 +0600

    added token auth imports

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.db.models.fields.reverse_related import ManyToOneRel
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class Student(models.Model):
